6aa21a514118ad95d93d598b68d048394733e0c8
DavidDaCoder <DavidDaCoder@users.noreply.github.com>
2026-07-27 16:45:28 +0000

    Publish rendered site (source commit 6bc6182cfa681934e424e40aad7953f871888c81)

---
+++ _astro/theme.CMGdozEt.js
@@ -0,0 +1,1 @@
+function o(t,e){return typeof window>"u"?e:getComputedStyle(document.documentElement).getPropertyValue(t).trim()||e}const i=()=>Math.min(window.devicePixelRatio,2);export{i as a,o as c};
